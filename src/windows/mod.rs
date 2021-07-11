174a9199e7bb5e6d5ca973210b3441251ffe8b5c
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2021-07-11 17:56:10 +0200

    Compress error handling Used a macro, to increase code readability.

--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -6,7 +6,7 @@ use std::ffi::{CString, CStr};
 use std::fs;
 use std::mem::size_of;
 use widestring::WideCString;
-use winapi::shared::minwindef::{DWORD, FALSE, MAX_PATH, FARPROC};
+use winapi::shared::minwindef::{DWORD, FALSE, MAX_PATH, FARPROC, HMODULE, __some_function};
 use winapi::um::errhandlingapi::GetLastError;
 use winapi::um::handleapi::{CloseHandle, INVALID_HANDLE_VALUE};
 use winapi::um::libloaderapi::{LoadLibraryA, LoadLibraryW, GetProcAddress};
@@ -16,56 +16,67 @@ use winapi::um::tlhelp32::{
     CreateToolhelp32Snapshot, Process32FirstW, Process32NextW, LPPROCESSENTRY32W, PROCESSENTRY32W,
     TH32CS_SNAPPROCESS,
 };
-use winapi::um::winnt::{MEM_COMMIT, MEM_DECOMMIT, PAGE_READWRITE, PROCESS_CREATE_THREAD, PROCESS_VM_OPERATION, PROCESS_VM_WRITE, MEM_RESERVE, PROCESS_ALL_ACCESS};
+use winapi::um::winnt::{MEM_COMMIT, MEM_DECOMMIT, PAGE_READWRITE, PROCESS_CREATE_THREAD, PROCESS_VM_OPERATION, PROCESS_VM_WRITE, MEM_RESERVE, PROCESS_ALL_ACCESS, PROCESS_QUERY_INFORMATION};
 use winapi::ctypes::{c_void, c_long, c_ulong};
 use scopeguard::guard;
 use winapi::um::synchapi::WaitForSingleObject;
 use winapi::um::winbase::INFINITE;
+
+type Error = (String,u32);
+type Result<T> = std::result::Result<T,Error>;
+
+macro_rules! check_ptr{
+    ($name:ident($($args:expr),*))=>{
+        {
+            match check_null_ptr(unsafe{$name($($args),*)},std::stringify!($name)) {
+                Ok(val) => val,
+                Err(err) => return Err(err),
+            }
+        }
+    };
+    ($name:ident($($args:expr),*),$guard:literal) => {
+            check_ptr!($name($($args),*),
+            |guard| {
+            info!("Cleaning {} Handle",$guard);
+            if unsafe { CloseHandle(guard) } == FALSE {
+                error!("Error during cleanup!");
+                //Supress unused_must_use warning. This is intended, but one cannot use allow, to supress this?
+                //todo: a bit hacky? Is there a better way, to achieve something similar?
+                match err::<()>(("CloseHandle of ".to_string()+std::stringify!($name)).as_str()){
+                    Ok(_)=>{},
+                    Err(_)=>{},
+                };
+                panic!("Error during cleanup")
+            }
+        }
+        )
+    };
+    ($name:ident($($args:expr),*),$guard:expr) => {
+        scopeguard::guard(check_ptr!($name($($args),*)),$guard)
+    };
+}
 
 impl<'a> Injector<'a> {
     ///Actually Inject the DLL.
     ///Notice:This implementation blocks, and waits, until the library is injected, or the injection failed.
     ///Return information is purely informational (for now)! It should not be relied upon, and may change in Minor updates.
     //TODO:If return is OK(()), is the lib always injected? If Return is Err(_), is there actually some error?
-    pub fn inject(&self) -> Result<(), (String, u32)> {
+    pub fn inject(&self) -> Result<()> {
         //TODO:Recheck this Fn, and all the winapi calls
         if self.pid == 0 {
             warn!("Supplied id is 0. Will not inject, as it is not supported by windows");
-            return Err(("PID is 0".to_string(), u32::MAX));
+            return err_str("PID is 0");
         }
     
         //from_str_unchecked should be fine. There are no null bytes, in that literal string.
         // let str = WideCString::from_str("kernel32.dll").unwrap().as_ptr();
-        let k32_handle = unsafe{LoadLibraryW(WideCString::from_str("kernel32.dll").unwrap().as_ptr())};
-        if k32_handle.is_null(){
-            return Err(err("LoadLibraryW".to_string()));
-        }
+        let k32_handle = check_ptr!(LoadLibraryW(WideCString::from_str("kernel32.dll").unwrap().as_ptr()));
+        let load_library_a = check_ptr!(GetProcAddress(k32_handle,"LoadLibraryA".as_ptr() as *const i8));
 
-        let load_library_a = unsafe{GetProcAddress(k32_handle, b"LoadLibraryA\0".as_ptr() as *const i8)};
-        if load_library_a.is_null(){
-            return Err(err("GetProcAddress".to_string()));
-        }
-        
-        let proc = unsafe {
+        let proc = check_ptr!(
             OpenProcess(
-                // PROCESS_CREATE_THREAD | PROCESS_VM_WRITE | PROCESS_VM_OPERATION,
-                PROCESS_ALL_ACCESS,
-                FALSE,
-                self.pid,
-            )
-        };
-        let proc = guard(proc,
-        |proc| {
-            info!("Cleaning Process Handle");
-            if unsafe { CloseHandle(proc) } == FALSE {
-                error!("Error during cleanup!");
-                err("CloseHandle".to_string());
-                panic!("Error during cleanup")
-            }
-        });
-        if proc.is_null() {
-            return Err(err("OpenProcess".to_string()));
-        }
+            PROCESS_CREATE_THREAD | PROCESS_VM_WRITE | PROCESS_VM_OPERATION | PROCESS_QUERY_INFORMATION,
+            FALSE,self.pid),"Process");
         info!("Process Handle is {:?}",*proc);
         {
             let full_path = match fs::canonicalize(self.dll) {
@@ -79,80 +90,27 @@ impl<'a> Injector<'a> {
                 error!("Path is bigger, than MAX_PATH!");
                 return Err(("Path Size is bigger, than MAX_PATH".to_string(),0))
             }
-            let addr = unsafe {
-                VirtualAllocEx(
+            let addr = check_ptr!(VirtualAllocEx(
                     *proc,
                     std::ptr::null_mut(),
                     path_size,
                     MEM_COMMIT | MEM_RESERVE,
-                    PAGE_READWRITE,
-                )
-            };
-            if addr.is_null() {
-                return Err(err("VirtualAllecEx".to_string()));
-            }
-            //Add Drop fn
-            let addr = guard(addr,|_|{
-                info!("Cleaning Allocated Memory in proc.");
-                if unsafe { VirtualFreeEx(*proc, addr, path_size, MEM_DECOMMIT) } == FALSE {
-                    error!("Error during cleanup!");
-                    err("VirtualFreeEx".to_string());
-                    panic!("Error during cleanup")
-                }
-            });
+                    PAGE_READWRITE
+                ),"Allocated Memory"
+            );
             info!("Address is {:?}",*addr);
             
             let mut n = 0;
-            // let cstr = match CString::new(full_path.as) {
-            //     Ok(buf) => buf,
-            //     Err(_) => {
-            //         return Err((
-            //             "The full dll path contained NULL bytes. That is not allowed in CStrings.".to_string(),
-            //             0,
-            //         ));
-            //     }
-            // };
-            //
-            // let buffer = cstr.as_ptr() as *const c_void;
             let bytes = full_path.as_bytes();
             if unsafe { WriteProcessMemory(*proc, *addr, bytes.as_ptr() as *const c_void, path_size, &mut n) } == FALSE {
-                return Err(err("WriteProcessMemory".to_string()));
+                return err("WriteProcessMemory");
             }
             trace!("Wrote {} bytes. path has {} bytes",n, bytes.len()*8);
            
             {
-                // let mut size:usize=0;
-                // if unsafe{
-                //     InitializeProcThreadAttributeList(
-                //         std::ptr::null_mut(),
-                //         0,
-                //         0,
-                //         &mut size as *mut usize
-                //     )
-                // } == FALSE {
-                //     err("InitializeProcThreadAttributeList n1".to_string());
-                //     error!("Not returning. The error above might be expected!");
-                // }
-                // let mut attr_list:PROC_THREAD_ATTRIBUTE_LIST = PROC_THREAD_ATTRIBUTE_LIST { dummy: std::ptr::null_mut() };
-                // let ptr_attr_list:LPPROC_THREAD_ATTRIBUTE_LIST = &mut attr_list as *mut PROC_THREAD_ATTRIBUTE_LIST;
-                // if unsafe{
-                //     InitializeProcThreadAttributeList(
-                //         ptr_attr_list,
-                //         0,
-                //         0,
-                //         &mut size as *mut usize
-                //     )
-                // } == FALSE {
-                //     return Err(err("InitializeProcThreadAttributeList n2".to_string()))
-                // }
-                // let ptr_attr_list = guard(ptr_attr_list,|list|{
-                //     unsafe{DeleteProcThreadAttributeList(list)}
-                // });
-                
-                // let starting_point = unsafe{std::mem::transmute(LoadLibraryW as FARPROC)};
                 let starting_point = unsafe{std::mem::transmute(load_library_a)};
                 let mut thread_id:u32=0;
-                let thread = unsafe {
+                let thread = check_ptr!(
                     CreateRemoteThread(
                         *proc,
                         std::ptr::null_mut(),
@@ -162,28 +120,15 @@ impl<'a> Injector<'a> {
                         0,
                         // *ptr_attr_list,
                         &mut thread_id as *mut u32
-                    )
-                };
+                    ),"thread");
                 let thread_id = thread_id;
-                if thread.is_null() {
-                    return Err(err("CreateRemoteThread".to_string()));
-                }
-                let thread = guard(thread,
-                                 |thread| {
-                                     info!("Cleaning thread Handle");
-                                     if unsafe { CloseHandle(thread) } == FALSE {
-                                         error!("Error during cleanup!");
-                                         err("CloseHandle".to_string());
-                                         panic!("Error during cleanup")
-                                     }
-                                 });
                 info!("Thread is {:?} and thread id is {}", *thread, thread_id);
                 info!("Waiting for DLL");
                 match unsafe{WaitForSingleObject(*thread,INFINITE)}{
-                    0x80=>{return Err(err_str("WaitForSingleObject returned WAIT_ABANDONED".to_string()))},//WAIT_ABANDONED
+                    0x80=>{return err_str("WaitForSingleObject returned WAIT_ABANDONED")},//WAIT_ABANDONED
                     0x0=>{info!("Dll inject success? IDK?! Hopefully? WaitForSingleObject returned WAIT_OBJECT_0");return Ok(())},//WAIT_OBJECT_0
-                    0x102=>{return Err(err_str("Timeout hit at WaitForSingleObject.".to_string()))},//WAIT_TIMEOUT
-                    0xFFFFFFFF=>{return Err(err("WaitForSingleObject".to_string()))},//WAIT_FAILED
+                    0x102=>{return err_str("Timeout hit at WaitForSingleObject.")},//WAIT_TIMEOUT
+                    0xFFFFFFFF=>{return err("WaitForSingleObject")},//WAIT_FAILED
                     _=>{}
                 }
                 
@@ -193,14 +138,14 @@ impl<'a> Injector<'a> {
         Ok(())//todo: is this always ok? is this even hit?
     }
     ///Find a PID, where the process-name matches some user defined selector
-    pub fn find_pid_selector<F>(select: F) -> Result<Vec<u32>, (String, u32)>
+    pub fn find_pid_selector<F>(select: F) -> Result<Vec<u32>>
     where
         F: Fn(&String) -> bool,
     {
         let mut pids: Vec<DWORD> = Vec::new();
         let snap_handle = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0) };
         if snap_handle == INVALID_HANDLE_VALUE {
-            return Err(err("CreateToolhelp32Snapshot".to_string()));
+            return err("CreateToolhelp32Snapshot");
         }
         trace!("Got Snapshot of processes");
         let mut val = PROCESSENTRY32W {
@@ -222,7 +167,7 @@ impl<'a> Injector<'a> {
         let entry: LPPROCESSENTRY32W = &mut val as *mut PROCESSENTRY32W;
 
         if unsafe { Process32FirstW(snap_handle, entry) == FALSE } {
-            return Err(err("Process32FirstW".to_string()));
+            return err("Process32FirstW");
         }
 
         trace!("Ran Process32FirstW");
@@ -249,15 +194,22 @@ impl<'a> Injector<'a> {
     }
 }
 
-fn err(fn_name: String) -> (String, u32) {
+fn check_null_ptr<T>(ptr:*mut T,fn_name:&str) -> Result<*mut T>{
+    if ptr.is_null(){
+        return err(fn_name);
+    }
+    Ok(ptr)
+}
+
+fn err<T>(fn_name: &str) -> Result<T> {
     {
         let err = unsafe { GetLastError() };
         error!("{} failed! Errcode is:'{}'. Check, what the error code means here:'https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes'", fn_name, err);
-        (fn_name, err)
+        Err((fn_name.to_string(), err))
     }
 }
 
-fn err_str(err:String) -> (String, u32) {
+fn err_str<T>(err:&str) -> Result<T> {
     error!("{}",err);
-    (err,0)
+    Err((err.to_string(),0))
 }
