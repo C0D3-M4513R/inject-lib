af7fef62213718b9f3f60ecc351139dbbf9d2d91
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2022-03-15 23:43:31 +0100

    Add github configs
    Remove an unused (and broken) macro

--- Readme.md
+++ Readme.md
@@ -2,7 +2,7 @@
 This library(will later be a crate) exists, to inject a dll into a windows process.
 This injector currently supports injecting from x64 into x86 and x64, and injecting from x86 into x86 (and x64 [if compiled with correct features]).
 
-# Todos:
+## Todos:
 
 - [ ] Find a good name
 - [x] Make the injector work also on x86->x64
--- src/platforms/windows/macros.rs
+++ src/platforms/windows/macros.rs
@@ -14,27 +14,6 @@ use winapi::um::errhandlingapi::GetLastError;
 #[inline]
 pub fn void_res<T>(_: Result<T>) {}
 
-macro_rules! guard_check_ptr {
-    ($name:ident($($args:expr),*),$guard:literal) => {
-            guard_check_ptr!($name($($args),*),
-            |guard| {
-            trace!("Cleaning {} Handle",$guard);
-            if unsafe { CloseHandle(guard) } == FALSE {
-                error!("Error during cleanup!");
-                //Supress unused_must_use warning. This is intended, but one cannot use allow, to supress this?
-                //todo: a bit hacky? Is there a better way, to achieve something similar?
-                $crate::platforms::platform::macros::void_res($crate::platforms::platform::macros::err::<(),String>(("CloseHandle of ".to_string()+std::stringify!($name))));
-                panic!("Error during cleanup");
-            }
-        }
-        )
-    };
-    ($name:ident($($args:expr),*),$guard:expr) => {
-        scopeguard::guard($crate::platforms::platform::check_ptr!($name($($args),*)),$guard)
-    };
-}
-pub(crate) use guard_check_ptr;
-
 macro_rules! check_ptr {
     ($name:ident($($args:expr),*),$predicate:expr)=>{
         {
@@ -54,10 +33,12 @@ pub(crate) use check_ptr;
 
 ///Gets the windows Error, prints it, and returns an error.
 pub(crate) fn err<T, E>(fn_name: E) -> Result<T>
-	where E: Display {
-	let err = unsafe { GetLastError() };
-	error!("{} failed! Errcode is:'{}'. Check, what the error code means here:'https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes'", fn_name, err);
-	Err((fn_name.to_string(), err))
+where
+    E: Display,
+{
+    let err = unsafe { GetLastError() };
+    error!("{} failed! Errcode is:'{}'. Check, what the error code means here:'https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes'", fn_name, err);
+    Err((fn_name.to_string(), err))
 }
 
 ///Checks a NtStatus, using [check_nt_status].
